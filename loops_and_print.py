529377a386b37daaa154ec624af9519e2c8273da
Martiniano <140777358+MartiNogueira@users.noreply.github.com>
2024-10-15 16:32:37 -0300

    Update loops_and_print.py

--- loops_and_print.py
+++ loops_and_print.py
@@ -1,6 +1,23 @@
-def enumerate_list(list):
-    return list
+def enumerate_list(strings):
+    result = []
+    count = 0  
+    for index, color in enumerate(strings):
+        if color:  # Solo toma los valores que no están vacíos
+            result.append(f"{count}. {color}")
+            count += 1  # Incrementa el contador solo para los strings no vacíos
+    return result
+
+
+
+
+
 
 
 def enumerate_backwards(list):
-    return list
+    result = []
+    count = 0
+    for index, value in enumerate(list):
+        if value:  #Solo toma los valores que no estan vacios 
+            result.append(f"{count}. {value[::-1]}")
+            count += 1
+    return result

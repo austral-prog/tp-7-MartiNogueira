ecc39d436af5f1e5be709b2cb9b9188841b01a69
Martiniano <140777358+MartiNogueira@users.noreply.github.com>
2024-10-15 16:32:02 -0300

    Update loops.py

--- loops.py
+++ loops.py
@@ -1,18 +1,57 @@
-def index_of_by_index(word, list, index):
-    return -1
+def index_of_by_index(word, list, i):
+
+    if i < 0 or i >= len(list):
+        return -1
+    
+    for i in range(i, len(list)):
+        if list[i] == word:
+            return i  
+            
+    return -1 
+
+
+
 
 
 def index_of_empty(list):
+
+
+    for i, element in enumerate(list):
+        if element == "":
+            return i
     return -1
+
 
 
 def index_of(word, list):
-    return -1
+
+    if word in list:
+        return list.index(word)
+    else:
+        return -1 
 
 
-def put(word, list):
-    return -1
+def put(color, list1):
+    for i in range(len(list1)):
+        if list1[i] == "":  
+            list1[i] = color  
+            return i  
+    return -1  
+
+
 
 
 def remove(word, list):
-    return -1
+    count = 0
+    for i in range(len(list)):
+        if list[i] == word:
+            list[i] = ""  
+            count += 1   
+    return count 
+
+
+
+
+
+
+
